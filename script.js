7b1d861484bfa3e9a00391d2037644de04b32a8c
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-19 13:31:20 +0545

    Startng js learning in 4th sem hehe

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("we we we");
+console.log("we we we");
+console.log("we we we");
+console.log("we we we");
+
+var catVar= "cat var cat "
+let catLet= "cat let cat"
+const car= "car"
+console.log(catLet, "\n",car,"\n",catVar)
