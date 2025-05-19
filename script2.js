98b6851cd2792b00fcaf10544994cdf3ae2f9610
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-19 14:05:45 +0545

    functional and block scope

---
+++ script2.js
@@ -0,0 +1,29 @@
+console.log('--------------------------------------')
+console.log('--------------------------------------')
+function name(){
+    var age = 10;
+    console.log("age:",age)
+    if(106){ // same as if(true){...} 
+        var age = 20;
+        console.log("age:",age)
+    }
+    console.log("age:",age)
+}
+
+name();
+console.log('--------------------------------------')
+console.log('--------------------------------------')
+
+function names(){
+    let age = 10;
+    console.log("age:",age)
+    if(106){ // same as if(true){...} 
+        let age = 20;
+        console.log("age:",age)
+    }
+    console.log("age:",age)
+}
+names();
+
+console.log('--------------------------------------')
+console.log('--------------------------------------')
