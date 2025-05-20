6e307375d461a22b492315cd95dd418e7b30999d
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-20 13:51:28 +0545

    looll

--- day2/logical.js
+++ day2/logical.js
@@ -7,10 +7,10 @@ console.log(x&y);
 console.log(x|y);
 // console.log(x!y);
 
-age = 19;
+age=0;
 licenseHas = false;
 
 
-console.log('If licence',age>18&&licenseHas)
+console.log('If human',age>0||licenseHas)
 console.log('If eligible',age>18||licenseHas)
 console.log('If can drive',age>18&&licenseHas)
