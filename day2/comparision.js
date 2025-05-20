621aae167d43d397e21ce6b5badef0971f082328
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-20 13:18:26 +0545

    Comparision

---
+++ day2/comparision.js
@@ -0,0 +1,16 @@
+x= 10;
+y= 'ccn'
+z= 20;
+a=10;
+b="10";
+
+console.log(x==y,x==z,x==a,x==b);
+console.log(x===y,x===z,x===a,x===b);
+console.log(x!=y,x!=z,x!=a,x!=b);
+console.log(x!==y,x!==z,x!==a,x!==b);
+
+console.log("smaller than",x<y,x<z,x<a,x<b);
+console.log("Greater than",x>y,x>z,x>a,x>b);
+
+console.log(parseInt(y));
+console.log(parseInt(b));
