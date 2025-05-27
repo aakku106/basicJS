629aa321392d622f0505678ead5f7b149728ebd4
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-27 11:16:30 +0545

    addded
    - toogle button logic
    - reset button logic

--- day6/hw.js
+++ day6/hw.js
@@ -1,13 +1,23 @@
+/** @format */
+
 const colorScheme = document.body;
 var count = 0;
 const counter = document.getElementById("counter");
 counter.innerHTML = count;
+const button = document.getElementsByTagName("button");
+
 toggleMode = () => {
   colorScheme.classList.toggle("dark");
+  for (i of button) {
+    i.classList.toggle("dark");
+  }
 };
 add = () => {
   counter.innerText = ++count;
 };
 sub = () => {
   counter.innerText = --count;
+};
+reset = () => {
+  counter.innerText = 0;
 };
