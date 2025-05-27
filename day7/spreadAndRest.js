3ec8e2f8f1c1dd74b369280db1e57058c4ee8a6b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-27 14:25:46 +0545

    Merge

--- day7/spreadAndRest.js
+++ day7/spreadAndRest.js
@@ -1,4 +1,11 @@
+//Spread
 let arr = [1, 2, 3, 4, 5];
 let newArr = [...arr];
 console.log(newArr);
-console.log(arr);
+
+//merge
+let A = [1, 2];
+let B = [3, 4];
+let c = [...A, ...B];
+
+console.log(c);
