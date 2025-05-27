fb519aaef25ba2fc9f8bb839ce71c701a9750b45
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-27 14:22:41 +0545

    Array and object distruction

---
+++ day7/another.js
@@ -0,0 +1,18 @@
+//array descturing
+//
+let arr = [1, 2, 3];
+
+let [x, y] = arr;
+console.log(x);
+console.log(y);
+
+//object descturing
+
+let obj = {
+  names: "ccn",
+  age: 16,
+  classes: 6,
+};
+let { names, age, classes } = obj;
+
+console.log(names);
