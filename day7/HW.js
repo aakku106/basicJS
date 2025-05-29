8386af671e21d0939eef729b4d27aeab5b5dae89
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-29 23:43:31 +0545

    Overall optimizatiojn and bugFixes

--- day7/HW.js
+++ day7/HW.js
@@ -3,6 +3,9 @@ const showError = document.getElementById("showError");
 const password = document.getElementById("password");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
+const ui = document.getElementById("showDetails");
+const holder = document.getElementById("holder");
+
 let store = {
   username: "",
   email: "",
@@ -10,28 +13,51 @@ let store = {
 };
 
 showError.style.display = "none";
+showError.style.color = "red";
+ui.style.display = "none";
+holder.style.display = "none";
 let errorText = "";
-SubmitDetails = () => {
+let showChange = "";
+
+const SubmitDetails = (e) => {
+  e.preventDefault();
   errorText = "";
   showError.style.display = "none";
   if (username.value.trim().length < 4) {
-    errorText += "\nUser name shall be more than 4 character\n";
+    errorText += "User name shall be more than 4 character <br>";
+    username.style.border = "1px solid red";
+  } else username.style.border = "none";
+  if (!email.checkValidity()) {
+    errorText += "Please enter a valid email address.<br>";
+    email.style.border = "1px solid red";
+  } else email.style.border = "none";
+
+  if (password.value.trim().length < 8) {
+    errorText += "Password Must be more than 8 character <br>";
+    password.style.border = "1px solid red";
+  } else password.style.border = "none";
+
+  if (errorText !== "") {
     showError.innerHTML = errorText;
     showError.style.display = "block";
-  } else if (password.value.trim().length < 8) {
-    errorText += "\nPassword Must be more than 8 character\n";
-    showError.innerHTML = errorText;
-    showError.style.display = "block";
+    return;
   } else {
     store.username = username.value;
     store.email = email.value;
     store.password = password.value;
-    uiShow();
+    username.value = "";
+    email.value = "";
+    password.value = "";
   }
 };
+const uiShow = () => {
+  showChange = `username: ${store.username} <br> email: ${store.email} <br>  password: ${store.password}`;
+  ui.innerHTML = showChange;
+  ui.style.display = "block";
+  holder.style.display = "block";
+};
 
-uiShow = () => {
-  alert(
-    `Success\n\n username:${username.value}\temail:\t${email.value}\n password:\t${password.value}`
-  );
+const hideUI = () => {
+  ui.style.display = "none";
+  holder.style.display = "none";
 };
