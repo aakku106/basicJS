0df8b9c4b1090f58c80212f8adff01e2b1bb0a84
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-03 09:28:12 +0545

    Some basic trim value change

--- day7/sanjitaKoCode/script.js
+++ day7/sanjitaKoCode/script.js
@@ -1,3 +1,5 @@
+/** @format */
+
 let div = document.getElementById("container");
 div.style.backgroundColor = "#607d8b24";
 div.style.height = "90vh";
@@ -12,45 +14,49 @@ h1.style.fontSize = "50px";
 const but = document.getElementById("showbtn");
 const form = document.getElementById("register");
 const message = document.getElementById("error");
-const name = document.getElementById("name").value.trim();
-const email = document.getElementById("email").value.trim();
-const password = document.getElementById("password").value.trim();
-const confirmPassword = document.getElementById("confirm").value.trim();
+const names = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (!name || !email || !password || !confirmPassword) {
-    message.textContent = "Please fill in all fields.";
-    return;
-  }
-
-  if (password !== confirmPassword) {
+  if (password.value !== confirmPassword.value) {
     message.textContent = "Passwords do not match.";
     return;
   }
 
-  if (name.length < 4) {
+  if (names.value.length < 4) {
     message.textContent = "name must have 4 letters";
     message.style.display = "block";
     return;
   }
 
-  if (password.length < 8) {
+  if (password.value.length < 8) {
     message.textContent = "password must contain 8 letters";
     message.style.display = "block";
     return;
   }
+  if (
+    !names.value ||
+    !email.value ||
+    !password.value ||
+    !confirmPassword.value
+  ) {
+    message.textContent = "Please fill in all fields.";
+    return;
+  }
 
   message.textContent = "";
-  updateUI(name, email);
+  updateUI(names, email);
   form.reset();
 });
 
 but.addEventListener("click", function () {
   // register.classList.toggle("hidden");
   //  register.classList.toggle("visible");
-  updateUI();
+  updateUI(names, email);
 });
 
 function updateUI(name, email) {
