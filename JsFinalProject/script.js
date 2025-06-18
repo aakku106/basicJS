06fb5949a10b5f37acd7bbe9ad83da9b61acdd4a
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-18 10:04:21 +0545

    Refactor quiz logic: encapsulate answer handling and update question display

--- JsFinalProject/script.js
+++ JsFinalProject/script.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import { quiz } from "./question.js";
 import {
   body,
@@ -13,135 +15,73 @@ import {
 } from "./stylings.js";
 import { timer, timerDiv, timerInterval } from "./timer.js";
 
-export let points = 0;
 export const startTest = () => {
+  let i = 0;
+  let points = 0;
   timer();
   header.append(timerDiv);
   progressBar.value = 1;
 
-  pragraph.innerHTML = `${quiz[0].question}`;
+  bttn1.addEventListener("click", handleAnswer);
+  bttn2.addEventListener("click", handleAnswer);
+  bttn3.addEventListener("click", handleAnswer);
+  bttn4.addEventListener("click", handleAnswer);
 
-  bttn1.innerHTML = `${quiz[0].options[1]}`;
-  bttn2.innerHTML = `${quiz[0].options[2]}`;
-  bttn3.innerHTML = `${quiz[0].options[3]}`;
-  bttn4.innerHTML = `${quiz[0].options[4]}`;
+  updateQuestion();
 
-  section.append(bttn1);
-  section.append(bttn2);
-  section.append(bttn3);
-  section.append(bttn4);
-  body.append(pragraph);
-  body.append(section);
-  bttn1.addEventListener("click", () => {
-    loader.style.display = "block";
-    section.style.display = "none";
-    pragraph.style.display = "none";
-
-    if (bttn1.innerHTML === quiz[0].answer) {
-      points++;
-      console.log(points);
-      loader.style.display = "block";
-      section.style.display = "none";
-      pragraph.style.display = "none";
-    }
-  });
-  bttn2.addEventListener("click", () => {
-    loader.style.display = "block";
-    section.style.display = "none";
-    pragraph.style.display = "none";
-
-    if (bttn2.innerHTML === quiz[0].answer) {
-      points++;
-      console.log(points);
-    }
-  });
-  bttn3.addEventListener("click", () => {
-    loader.style.display = "block";
-    section.style.display = "none";
-    pragraph.style.display = "none";
-
-    if (bttn3.innerHTML === quiz[0].answer) {
-      points++;
-      console.log(points);
-    }
-  });
-  bttn4.addEventListener("click", () => {
-    loader.style.display = "block";
-    section.style.display = "none";
-    pragraph.style.display = "none";
-
-    if (bttn4.innerHTML === quiz[0].answer) {
-      points++;
-      console.log(points);
-    }
-  });
-
-  let i = 1;
   let interval = setInterval(() => {
     clearInterval(timerInterval);
     timer();
     header.append(timerDiv);
-    progressBar.value = i;
-    if (i > quiz.length - 2) {
+
+    i++;
+    progressBar.value = i + 1;
+
+    if (i >= quiz.length) {
       clearInterval(interval);
+      clearInterval(timerInterval);
+      showResults();
       return;
     }
+
+    updateQuestion();
+  }, 6000);
+
+  const updateQuestion = () => {
     loader.style.display = "none";
     section.style.display = "block";
     pragraph.style.display = "block";
 
-    pragraph.innerHTML = `${quiz[i].question}`;
-    bttn1.innerHTML = `${quiz[i].options[1]}`;
-    bttn2.innerHTML = `${quiz[i].options[2]}`;
-    bttn3.innerHTML = `${quiz[i].options[3]}`;
-    bttn4.innerHTML = `${quiz[i].options[4]}`;
+    pragraph.innerHTML = quiz[i].question;
+    bttn1.innerHTML = quiz[i].options[1];
+    bttn2.innerHTML = quiz[i].options[2];
+    bttn3.innerHTML = quiz[i].options[3];
+    bttn4.innerHTML = quiz[i].options[4];
+
+    section.innerHTML = "";
     section.append(bttn1);
     section.append(bttn2);
     section.append(bttn3);
     section.append(bttn4);
     body.append(pragraph);
     body.append(section);
-    bttn1.addEventListener("click", () => {
-      loader.style.display = "block";
-      section.style.display = "none";
-      pragraph.style.display = "none";
+  };
 
-      if (quiz[i].options[1] === quiz[i].answer) {
-        points++;
-        console.log(points);
-      }
-    });
-    bttn2.addEventListener("click", () => {
-      loader.style.display = "block";
-      section.style.display = "none";
-      pragraph.style.display = "none";
+  const handleAnswer = (e) => {
+    loader.style.display = "block";
+    section.style.display = "none";
+    pragraph.style.display = "none";
 
-      if (quiz[i].options[2] === quiz[i].answer) {
-        points++;
-        console.log(points);
-      }
-    });
-    bttn3.addEventListener("click", () => {
-      loader.style.display = "block";
-      section.style.display = "none";
-      pragraph.style.display = "none";
+    if (e.target.innerHTML === quiz[i].answer) {
+      points++;
+      console.log("Points: " + points);
+    }
+  };
 
-      if (quiz[i].options[3] === quiz[i].answer) {
-        points++;
-        console.log(points);
-      }
-    });
-    bttn4.addEventListener("click", () => {
-      loader.style.display = "block";
-      section.style.display = "none";
-      pragraph.style.display = "none";
-
-      if (quiz[i].options[4] === quiz[i].answer) {
-        points++;
-        console.log(points);
-      }
-    });
-
-    i++;
-  }, 6000);
+  const showResults = () => {
+    section.innerHTML = "";
+    pragraph.innerHTML = `Quiz Complete! Your score: ${points} out of ${quiz.length}`;
+    timerDiv.style.display = "none";
+    loader.style.display = "none";
+  };
 };
