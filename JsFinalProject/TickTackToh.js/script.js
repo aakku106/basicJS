b9c35052a9903bd9d8f232dc9cf836418acb4587
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-19 15:18:44 +0545

    Tick tack tho menually menapulating do hehe..

---
+++ JsFinalProject/TickTackToh.js/script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.getElementById("main");
+  let container = document.createElement("section");
+  body.append(container);
+  container.className = "cointainer";
+  let row1 = document.createElement("div");
+  let row2 = document.createElement("div");
+  let row3 = document.createElement("div");
+  container.append(row1, row2, row3);
+  let box1 = document.createElement("span");
+  let box2 = document.createElement("span");
+  let box3 = document.createElement("span");
+  row1.append(box1, box2, box3);
+  let box4 = document.createElement("span");
+  let box5 = document.createElement("span");
+  let box6 = document.createElement("span");
+  row2.append(box4, box5, box6);
+  let box7 = document.createElement("span");
+  let box8 = document.createElement("span");
+  let box9 = document.createElement("span");
+  row3.append(box7, box8, box9);
+
+  row1.id = "row1";
+  row2.id = "row2";
+  row3.id = "row3";
+  box1.id = "box1";
+  box2.id = "box2";
+  box3.id = "box3";
+  box4.id = "box4";
+  box5.id = "box5";
+  box6.id = "box6";
+  box7.id = "box7";
+  box8.id = "box8";
+  box9.id = "box9";
+
+  let turn = "X";
+  let arr = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  const handleClick = (e) => {
+    if (e.target.textContent == "") {
+      if (turn === "X") {
+        e.target.innerHTML = turn;
+        console.log(e);
+        turn = "O";
+        check(e);
+      } else {
+        e.target.innerHTML = turn;
+        console.log(e);
+        turn = "X";
+      }
+    } else return;
+  };
+
+  box1.addEventListener("click", handleClick);
+  box2.addEventListener("click", handleClick);
+  box3.addEventListener("click", handleClick);
+  box4.addEventListener("click", handleClick);
+  box5.addEventListener("click", handleClick);
+  box6.addEventListener("click", handleClick);
+  box7.addEventListener("click", handleClick);
+  box8.addEventListener("click", handleClick);
+  box9.addEventListener("click", handleClick);
+
+  const check = (e) => {
+    if (e.target.id === "box1") arr[0][0] = 1;
+    console.log(arr);
+  };
+});
